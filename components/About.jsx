f417ade325086af32579fae41e15e10c2aa04824
Umer Ahmed <umerahmed4121@gmail.com>
2024-01-05 21:03:54 +0500

    Initial commit

---
+++ components/About.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='mt-[25vh]'>
+        <h2 className='heading_2'>About</h2>
+        <p className='paragraph'>Welcome to AssessAI, your go-to platform for cutting-edge automated answer script assessment. We understand the challenges in grading and evaluating responses in educational settings, and AssessAI is here to transform the way you assess, saving time and ensuring accuracy.</p>
+        <h3 className='heading_3'>Key Features</h3>
+        <ul className='px-6 mt-6 font-normal list-disc'>
+            <li>Efficiency: Say goodbye to manual grading. Our automated system swiftly evaluates answer scripts, providing quick and accurate results.</li>
+            <li>Accuracy: Harness the power of AI to eliminate human bias, ensuring fair and consistent evaluations every time.</li>
+            <li>Customization: Tailor assessments to your curriculum and criteria, ensuring a personalized and effective evaluation process.</li>
+            <li>Insightful Analytics: Gain valuable insights into student performance with detailed analytics and reports, helping you make informed decisions for the future.</li>
+        </ul>
+        <h3 className='heading_3'>Why Choose AssessAI?</h3>
+        <p className='paragraph'>AssessAI is not just a platform; it's a revolution in education assessment. Whether you are an educator, institution, or organization, our platform adapts to your needs, streamlining the assessment process and enhancing the overall learning experience.</p>
+        <h4 className='heading_4'>For Educators:</h4>
+        <p className='paragraph ml-4'>Empower your teaching with AssessAI. Focus more on inspiring and guiding your students, while we handle the assessment workload.</p>
+        <h4 className='heading_4'>For Institutions:</h4>
+        <p className='paragraph ml-4'>Elevate the standards of your assessments, ensuring fair evaluations and efficient management of academic data.</p>
+        <h4 className='heading_4'>For Organizations:</h4>
+        <p className='paragraph ml-4'>Make informed decisions about employee training and development with our reliable assessment platform.</p>
+    </section>
+  )
+}
+
+export default About
---
+++ components/Main.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+/* 
+
+sm: 640px
+    padding-top: 4vh;
+    padding-left: 6vw;
+    padding-right: 1vw;
+md: 768px
+lg: 1024px
+xl: 1280px
+
+
+*/
+
+const Main = () => {
+  return (
+    <div className='bg-[url("/assets/background-mobile.jpg")] sm:bg-[url("/assets/background.jpg")] w-full h-[94vh] sm:h-[100vh] bg-contain bg-bottom sm:bg-right-bottom bg-no-repeat'>
+        <div className='pt-[4vh] pl-[6vw] pr-[1vw] md:pt-[15vh]  lg:pt-[28vh] lg:pl-[10vh]'>
+           <h1 className='text-3xl sm:text-6xl font-bold'>
+            Welcome to AssessAI:
+            <br />
+            <span>Revolutionizing Education Assessment</span>
+            </h1> 
+            <p className='mt-12 text-lg lg:text-2xl font-medium'>Unlock the Future of Learning with Automated Answer Script Assessment!</p>
+            
+        </div>
+        
+        
+    </div>
+  )
+}
+
+export default Main
