1a85c3bf68562c61186fb2a02776b3123033f77e
Umer Ahmed Siddiqui <umerahmed4121@gmail.com>
2024-02-14 23:25:50 +0500

    till auth

--- components/Dashboard/index.jsx
+++ components/Dashboard/index.jsx
@@ -4,12 +4,12 @@ import Loader from '../Loader';
 
 const Row = () => {
     return (
-        <div className='grid grid-cols-[64px,100px,300px,auto,16px] justify-items-center items-center gap-16'>
-            <div className='bg-slate-200 w-16 h-16 rounded-full'></div>
+        <div className='grid grid-cols-[30%,70%] sm:grid-cols-[64px,100px,300px,auto,16px] justify-items-center items-center px-4 gap-1 sm:gap-16'>
+            <div className='bg-slate-200 w-10 h-10 sm:w-16 sm:h-16 rounded-full'></div>
             <div className='bg-slate-200 w-full h-4 rounded-3xl'></div>
-            <div className='bg-slate-200 w-full h-4 rounded-3xl'></div>
-            <div className='bg-slate-200 w-full h-4 rounded-3xl'></div>
-            <div className='bg-slate-200 w-4 h-4 rounded'></div>
+            <div className='bg-slate-200 w-full h-4 rounded-3xl hidden sm:grid'></div>
+            <div className='bg-slate-200 w-full h-4 rounded-3xl hidden sm:grid'></div>
+            <div className='bg-slate-200 w-4 h-4 rounded hidden sm:grid'></div>
 
         </div>
     )
@@ -18,13 +18,21 @@ const Row = () => {
 const Dashboard = () => {
     return (
         <div className='dashboard_container '>
-            <Loader visible={true}/>
-            
-            <div className='grid gap-12 animate-pulse'>
+            <Loader visible={true} />
+
+            <div className='hidden sm:grid gap-12 animate-pulse'>
                 {[...Array(5)].map((_, i) => (
                     <Row key={i} />
                 ))}
             </div>
+
+            <div className='grid sm:hidden gap-12 animate-pulse'>
+                {[...Array(5)].map((_, i) => (
+                    <Row key={i} />
+                ))}
+            </div>
+
+
         </div>
     )
 }
